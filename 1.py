12f32fec449eb12af3b0d9fccd35499a950c5ffe
amitmalav <amitmalav96@gmail.com>
2016-03-08 19:33:13 +0530

    problems 1-10

--- 1.py
+++ 1.py
@@ -1,5 +1,39 @@
-count = 0
-for num in range(1,1000):
-	if (num % 3 == 0) or (num % 5 == 0):
-		count = count + num
-print count
+def gcd(a, b):
+	if(b == 0):
+		return a
+	return gcd(b, a%b)
+	# while(b != 0):
+	# 	tmp = a % b
+	# 	a = b
+	# 	b = tmp
+	# return a
+def hardcode():
+	count = 0
+	for num in range(1, 1000):
+		if (num % 3 == 0) or (num % 5 == 0):
+			count += num
+	return count
+
+
+#we can also write it as some of (S3+S5-S15) ie.
+
+
+# count = 3*int(999/3)*(1 + int(999/3))/2 + 5*int(999/5)*(1 + int(999/5))/2 - 15*int(999/15)*(1 + int(999/15))/2
+# print count
+
+
+
+def generalsum(a, b, below):
+	below = below - 1
+	lcm = (a/gcd(a,b))*b
+	count = a*int(below/a)*(1 + int(below/a))/2 + b*int(below/b)*(1 + int(below/b))/2 - lcm*int(below/lcm)*(1 + int(below/lcm))/2
+	return count
+
+if __name__ == "__main__":
+	print "a is "
+	a = input()
+	print "b is "
+	b = input()
+	print "range is "
+	below = input()
+	print "sum is:",generalsum(a, b, below)
--- test.py
+++ test.py
@@ -1,14 +1,14 @@
-i = 2;
-num1 = 1
-num2 = 2
-n = input("Inter n: ")
-if n == 1:
-	print(num1)
-else:
-	while i < n:
-		tmp = num2
-		num2 = num2 + num1
-		num1 = tmp
-		i += 1
+def gcd(a, b):
+	if(b == 0):
+		return a
+	return gcd(b, a%b)
+	# while(b != 0):
+	# 	tmp = a % b
+	# 	a = b
+	# 	b = tmp
+	# return a
 
-	print(num2)
+if __name__ == "__main__":
+	a = input()
+	b = input()
+	print gcd(a, b)
