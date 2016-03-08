3c9cf5a031487a58081da6be141fd6b47752f0f3
amitmalav <amitmalav96@gmail.com>
2016-03-08 20:04:41 +0530

    efficient

--- 9.py
+++ 9.py
@@ -1,10 +1,13 @@
 import math
-for c in range(100, 0, -1):
-	for b in range(c-1, 0, -1):
-		for a in range(b-1, 0, -1):
-			if (a + b + c) == 100 and (a**2 + b**2 == c**2):
-				print(a*b*c)
-				break
+# for c in range(1000, 0, -1):
+# 	for b in range(c-1, 0, -1):
+# 		for a in range(b-1, 0, -1):
+# 			if (a + b + c) == 1000 and (a**2 + b**2 == c**2):
+# 				print a
+# 				print b
+# 				print c
+# 				print(a*b*c)
+# 				break
 
 
 """
@@ -22,3 +25,21 @@ m= 20; n= 5;
 
 a= 200; b= 375; c= 425;
 """
+
+def pythtrip(l):
+	for m in range(int(math.sqrt(l)), 0, -1):
+		for n in range(m-1, 0, -1):
+			if 2*m*(m + n) == l:
+				a = 2*m*n
+				b = m**2 - n**2
+				c = m**2 + n**2
+				product = a*b*c
+				print "a =", a
+				print "b =", b
+				print "c =", c
+				print "product is:", product
+
+if __name__ == "__main__":
+	print "give input"
+	l = input()
+	pythtrip(l)
